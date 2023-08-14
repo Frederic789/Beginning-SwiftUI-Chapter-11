0c27103ceb8984dcd870aafcf15164fb42014d1c
Frederic789 <113332830+Frederic789@users.noreply.github.com>
2023-08-13 23:36:29 -0700

    Update Map.swift

--- Chapter11/Map.swift
+++ Chapter11/Map.swift
@@ -1,4 +1,4 @@
-//
+//Modified version of map.swift
 //  ContentView.swift
 //  Chapter11
 //
@@ -8,14 +8,53 @@
 import SwiftUI
 
 struct MapExerciseView: View {
+    @State private var scale: CGFloat = 1.0
+    @GestureState private var magnification: CGFloat = 1.0
+    @GestureState private var rotation: Angle = .degrees(0)
+        @State private var accumulatedRotation: Angle = .degrees(0)
 
     
     var body: some View {
         VStack {
             Text("Map exercise goes here")
                 .font(.largeTitle)
+            
+            Image("map") // "mapImage" 
+                          .resizable()
+                          .aspectRatio(contentMode: .fit)
+                          .frame(height: 300) // Adjust the height as needed
+            
+                          .scaleEffect(scale) // Apply scale based on gesture
+                          .scaleEffect(scale * magnification) // Apply scale based on gesture
+                          .rotationEffect(rotation + accumulatedRotation)
+                                         
+                         
+                          
+                      Text("Explore the Map")
         }
         .padding()
+        
+        .gesture(
+                    MagnificationGesture()
+                        .onChanged { magnification in
+                            scale = magnification
+                        }
+                        .onEnded { _ in
+                            // Optional: Add any necessary cleanup after gesture ends
+                        }
+                )
+                .edgesIgnoringSafeArea(.all) // Ignore safe area for full-screen display
+        
+                .gesture(
+                            RotationGesture()
+                                .updating($rotation) { currentState, gestureState, transaction in
+                                    gestureState = currentState
+                                }
+                                .onEnded { finalRotation in
+                                    accumulatedRotation += finalRotation
+                                   
+                                }
+                        )
     }
 }
 
